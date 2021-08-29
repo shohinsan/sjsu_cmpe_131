1b8c1db0b4d10d18e2117f855606280dfa25a0da
shohinsan <abd.shohin@gmail.com>
2021-08-28 21:42:19 -0700

    Tested Bootstrap 5 as well as custom main.css and main.js files if they work.

--- alpha/projectdir/routes.py
+++ alpha/projectdir/routes.py
@@ -5,4 +5,12 @@ from flask import render_template
 @app.route('/')
 @app.route('/home')
 def homepage():  # put application's code here
-    return render_template("homepage.html")
+    return render_template("homepage.html", title='Home')
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template("about.html", title='About')
+
+@app.route('/account')
+def account():  # put application's code here
+    return render_template("account.html", title='About')
