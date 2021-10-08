609ca9df6d93201894dd7936c6378c70e5610bc5
shohinsan <abd.shohin@gmail.com>
2021-10-07 23:43:48 -0700

    Specific commit to pay attention to footer class

--- alpha/projectdir/forms.py
+++ alpha/projectdir/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
@@ -14,6 +14,7 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired(), Email()])
     password = PasswordField(label='Password', validators=[DataRequired(), Length(min=6, max=16)])
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField(label='Login')
 
 
