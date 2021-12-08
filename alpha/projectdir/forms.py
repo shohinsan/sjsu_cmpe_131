85fcb343cfb0aed8dad9786d661e1ec4b0d03184
hicbag <thomas.ng@sjsu.edu>
2021-12-07 21:21:18 -0800

    Added choosing which card to memorize for flashcard memorization

--- alpha/projectdir/forms.py
+++ alpha/projectdir/forms.py
@@ -66,4 +66,3 @@ class ShareForm(FlaskForm):
     username = StringField(label='Username', validators=[DataRequired(), Length(min=3, max=20)])
     email = StringField(label='Email', validators=[DataRequired(), Email()])
     submit = SubmitField(label="Submit")
-
